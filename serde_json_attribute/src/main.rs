56454b062cf93551112d47a77a1fdc12796c6bb8
marija-mijailovic <93908197+marija-mijailovic@users.noreply.github.com>
2024-02-25 22:25:44 +0100

    Add conditioanl serializing

--- serde_json_attribute/src/main.rs
+++ serde_json_attribute/src/main.rs
@@ -6,8 +6,8 @@ struct Token {
     token_id: u32,
     token_name: String,
     #[serde(default = "default_decimals")]
-    #[serde(skip_deserializing)]
     default_decimals: u32,
+    #[serde(skip_serializing_if = "not_is_process")]
     is_process: bool,
     amount: u32,
 }
@@ -16,18 +16,28 @@ fn default_decimals() -> u32 {
     18
 }
 
+fn not_is_process(value: &bool) -> bool {
+    !*value
+}
+
 fn main() {
-    let token = Token {
-        token_id: 1,
-        token_name: "Marija".to_owned(),
-        default_decimals: 8,
-        is_process: true,
-        amount: 100,
-    };
+    let token = vec![
+        Token {
+            token_id: 1,
+            token_name: "Marija".to_owned(),
+            default_decimals: 8,
+            is_process: false,
+            amount: 100,
+        },
+        Token {
+            token_id: 2,
+            token_name: "Mami".to_owned(),
+            default_decimals: 6,
+            is_process: true,
+            amount: 100,
+        },
+    ];
 
     let serialize = serde_json::to_string(&token).unwrap();
     println!("{}", serialize);
-
-    let deserialize: Token = serde_json::from_str(&serialize).unwrap();
-    println!("{:?}", deserialize);
 }
