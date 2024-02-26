6375e63ae808b445e54dfb9a5c1dc18f99ede3e3
marija-mijailovic <93908197+marija-mijailovic@users.noreply.github.com>
2024-02-26 13:57:01 +0100

    Add enum to serde_json example

--- serde_json/src/main.rs
+++ serde_json/src/main.rs
@@ -1,10 +1,19 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
+enum TokenType {
+    Native,
+    SBT,
+    NFT,
+    ERC20,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Token {
     id: u32,
     name: String,
     process: bool,
+    token_type: TokenType,
 }
 
 fn main() {
@@ -12,6 +21,7 @@ fn main() {
         id: 1,
         name: "Marija".to_owned(),
         process: true,
+        token_type: TokenType::NFT,
     };
 
     let serialize = serde_json::to_string(&token).unwrap();
